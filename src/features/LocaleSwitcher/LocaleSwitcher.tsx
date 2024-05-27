bdac4c74726f12a9da36d814b1d7d9396d252e4e
Andrii Hrynenko <pathliving@gmail.com>
2024-05-27 21:49:53 +0300

    refactor: deprecate langPath helper (in-022)

--- src/features/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/features/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,30 +1,23 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-// import { i18n, type Locale } from '../../../i18n-config';
-import langPath from '@/shared/lib/url/langPath';
-import { i18n } from '../../../i18n-config';
+import { useChangeLocale } from '@dictionaries/lib/client';
+import { i18n } from '@dictionaries/lib/constants';
 
 export default function LocaleSwitcher() {
-  const pathname = usePathname();
-  // const redirectedPathname = (locale: Locale) => {
-  //   if (!pathname) return '/';
-  //   const segments = pathname.split('/');
-  //   segments[1] = locale;
-  //   return segments.join('/');
-  // };
+  const changeLocale = useChangeLocale(/* { preserveSearchParams: true } */);
 
   return (
     <div>
-      <p>Locale switcher:</p>
       <ul>
         {i18n.locales.map((locale) => {
           return (
             <li key={locale}>
-              {/* <div>{redirectedPathname(locale)}</div> */}
-              {/* <Link href={redirectedPathname(locale)}>{locale}</Link> */}
-              <Link href={langPath(locale, pathname)}>{locale}</Link>
+              <button
+                type="button"
+                onClick={() => changeLocale(locale)}
+              >
+                {locale}
+              </button>
             </li>
           );
         })}
--- src/shared/lib/url/langPath.ts
+++ src/shared/lib/url/langPath.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated helper, use hook insted to get the current locale
+ */
 export default function langPath(locale: string, pathname: string) {
   if (!pathname) return '/';
   const segments = pathname.split('/');
