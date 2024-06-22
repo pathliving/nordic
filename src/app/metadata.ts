ef116c75297de250139974a37a0251583a3c27ec
Andrii Hrynenko <pathliving@gmail.com>
2024-06-22 18:56:49 +0300

    refactor: metadata (in-01)

---
+++ src/app/metadata.ts
@@ -0,0 +1,43 @@
+import { app } from '@/shared/config/app';
+import { social } from '@/shared/config/social';
+import { PAGE_HOME, PAGE_HOME_WITH_LOCALE } from '@/shared/constants/url';
+import { defaultLocale } from '@/shared/i18n/config';
+import { LOCALE_UK } from '@/shared/i18n/constants/locales';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: app.title,
+  description: app.description,
+  applicationName: app.applicationName,
+  metadataBase: app.url,
+  keywords: app.keywords,
+  referrer: app.referrer,
+  robots: app.robots,
+  alternates: {
+    canonical: app.url,
+    languages: {
+      [defaultLocale]: PAGE_HOME,
+      [LOCALE_UK]: PAGE_HOME_WITH_LOCALE,
+    },
+  },
+  icons: {
+    icon: app.icons?.favicon,
+    apple: app.icons?.appleIcon,
+    other: app.icons?.metadata,
+  },
+  manifest: app.manifest,
+  appleWebApp: {
+    capable: true,
+    title: app.title,
+    statusBarStyle: 'black-translucent',
+  },
+  openGraph: {
+    siteName: app.applicationName,
+    url: app.url,
+    ...social,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    ...social,
+  },
+};
